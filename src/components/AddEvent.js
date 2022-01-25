25876968b782f08ef71671b789b30910b38b2c81
unknown <abanardi0@gmail.com>
2022-01-25 00:36:27 -0500

    Add Event Panel updated

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -7,14 +7,33 @@ import Button from './Button';
 const AddEvent = ({ visible, setAddEventVisible }) => {
   return (
     <div className={visible ? 'add-event-panel' : 'add-event-panel invisible'}>
-      <h1 className="test">Header</h1>
-      <Button
-        className="add-event-close"
-        innerText="Close"
-        onClick={() => {
-          setAddEventVisible(false);
-        }}
-      />
+      <div className="top-panel">
+        <h1>ADD EVENT</h1>
+        <Button
+          className="add-event-close"
+          innerText="Close"
+          onClick={() => {
+            setAddEventVisible(false);
+          }}
+        />
+      </div>
+
+      <div className="bottom-panel">
+        <div className="date-selector">
+          <Button className='submit-form' innerText='Test Button'/>
+        </div>
+        <div className="right-panel">
+          <form className="submit-event">
+            <input type="time" />
+            <input
+              type="text"
+              className="event-description"
+              placeholder="Add Event"
+            />
+            <Button className='submit-form' innerText='Add'/>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
